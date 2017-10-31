ae1587b3fe75076fb7f7d1eb2b1b4f2db9df23ab
Igor Barbosa <igorbarbosa.p@gmail.com>
2017-10-31 01:19:32 -0200

    Add mirror_row function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -18,8 +18,15 @@ defmodule Identicon do
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
-    grid = Enum.chunk_every(hex, 3, 3, :discard)
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
 
     %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row([one, two, _three] = row) do
+    row ++ [two, one]
   end
 end
